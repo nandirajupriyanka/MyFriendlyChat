03d4d2f349db9c3924413764ef11769be7ebbf85
nandirajupriyanka <nandirajupriyanka@gmail.com>
2018-06-24 13:42:04 -0500

    feat: Implement facebook auth using FirebaseUI

--- app/src/main/java/com/priyankanandiraju/friendlychat/auth/AuthUIHelper.java
+++ app/src/main/java/com/priyankanandiraju/friendlychat/auth/AuthUIHelper.java
@@ -6,22 +6,28 @@ import android.content.Intent;
 import com.firebase.ui.auth.AuthUI;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by priyankanandiraju on 6/24/18.
  */
 
 public final class AuthUIHelper {
+
+    private static List<AuthUI.IdpConfig> getAuthProviders() {
+        return Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+                /*new AuthUI.IdpConfig.TwitterBuilder().build()*/);
+    }
+
     public static Intent getSignInIntent() {
         return AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setIsSmartLockEnabled(false)
-                .setAvailableProviders(Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-                        /*new AuthUI.IdpConfig.FacebookBuilder().build(),*/
-                        /*new AuthUI.IdpConfig.TwitterBuilder().build()*/))
+                .setAvailableProviders(getAuthProviders())
                 .build();
     }
 
